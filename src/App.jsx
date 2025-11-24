6e0d1a27deaf08cea352f42e68b340020e84694a
VictorNathan12 <victorpereiracamar@gmail.com>
2025-11-24 10:53:37 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,70 @@
-import styles from './App.module.css'
+import styles from "./App.module.css";
 
-import fotoPerfil from '/foto1.png'
+import fotoPerfil from "/foto1.png";
 
-import logo from '/LOGO.png'
-
+import logo from "/LOGO.png";
 
 function App() {
-  
+  const abrir = () => {
+    window.open("https://findect.org.br/dia-internacional-direitos-humanos/", "_blank");
+  }
 
   return (
     <>
-    
       <nav className={styles.menu}>
         <div>
           <img width={70} src={logo} alt="" />
         </div>
         <div>
-        <a href="#s1">Home</a>
-        <a href="#s2">Sobre</a>
+          <a href="#s1">Home</a>
+          <a href="#s2">Sobre</a>
         </div>
       </nav>
       <main>
-        <section className={styles.s1} id='s1'>
+        <section className={styles.s1} id="s1">
           <div className={styles.left}>
-            <img className={styles.imgPerfil} src={fotoPerfil} alt="Foto perfil" />
+            <img className={styles.imgPerfil} src={fotoPerfil} alt="" onClick={abrir}/>
           </div>
           <div className={styles.right}>
             <h2 className={styles.title}>Direitos Humanos</h2>
-            <p className={styles.paragraph}> São as garantias fundamentais para que todo ser humano possa viver com dignidade. Eles são inerentes a todas as pessoas, independentemente de raça, sexo, nacionalidade, etnia, religião ou qualquer outra condição.</p>
+            <p className={styles.paragraph}>
+              {" "}
+              Os Direitos Humanos são um conjunto de normas que reconhecem e
+              protegem a dignidade de todos os seres humanos. Eles pertencem a
+              todas as pessoas, simplesmente pelo fato de serem humanas, e são
+              universais, inalienáveis, indivisíveis e interdependentes.
+            </p>
           </div>
         </section>
-        
-        <section id="s2">
-            <h2>Sobre</h2>
-            <div class="wrap-cards">
-                <div class="card">
-                    <img src="./Images/" alt="Arthur Morgan"/>
-                    <h3>Arthur Morgan</h3>
-                    <p>Um membro da gangue Van der Linde, Arthur deve lidar com o declínio do Velho Oeste enquanto tenta sobreviver contra as forças do governo</p>
-                </div>
-            </div>
-       </section>
 
+        <section id="s2" className={styles.s2}>
+          <iframe
+            className={styles.largura}
+            src="https://www.youtube.com/embed/hGKAaVoDlSs?si=_XY25TcFb76fnjqr"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+
+          <div className={styles.second}>
+            <h2>Sobre</h2>
+            <p>
+              Direitos humanos são normas que reconhecem e protegem a dignidade
+              de todas as pessoas, garantindo que vivam de forma justa e
+              igualitária. Eles são universais, inalienáveis e intrínsecos a
+              cada indivíduo pelo simples fato de existir, regendo a relação
+              entre as pessoas e o Estado, que tem a obrigação de respeitá-los e
+              promovê-los através de leis e tratados. Os direitos humanos
+              abrangem áreas como vida, liberdade, igualdade, saúde, educação,
+              trabalho e segurança
+            </p>
+          </div>
+        </section>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
